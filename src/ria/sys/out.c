a7075039f8eee65526aabcbce97cf531a10942da
Tomasz Sterna <tomasz@sterna.link>
2024-11-11 16:32:31 +0100

    TMDS encoder RGB888 with double linebuffer

--- src/ria/sys/out.c
+++ src/ria/sys/out.c
@@ -17,10 +17,6 @@
 #include "main.h"
 #include "out.h"
 
-#include "../cgia/images/mountains_640x480_rgb332.h"
-#include <hardware/structs/clocks.h>
-#define framebuf mountains_640x480
-
 /** https://retrocomputing.stackexchange.com/a/13872
 > In standard bitmap mode the C64 outputs 320 pixels in 40µs.
 > The visible portion of a line is ~52µs; in 60Hz regions ~240 lines
@@ -59,6 +55,11 @@
 #define MODE_V_BACK_PORCH    6
 #define MODE_V_ACTIVE_LINES  480
 #define MODE_BIT_CLK_KHZ     266000
+
+// ----------------------------------------------------------------------------
+// RGB line buffers
+#define RGB_LINE_BUFFERS 2
+uint32_t linebuffer[MODE_H_ACTIVE_PIXELS * RGB_LINE_BUFFERS];
 
 // ----------------------------------------------------------------------------
 // DVI constants
@@ -176,8 +177,9 @@ void __scratch_x("") dma_irq_handler(void)
     }
     else
     {
-        ch->read_addr = (uintptr_t)&framebuf[(v_scanline - (MODE_V_TOTAL_LINES - MODE_V_ACTIVE_LINES)) * MODE_H_ACTIVE_PIXELS];
-        ch->transfer_count = MODE_H_ACTIVE_PIXELS / sizeof(uint32_t);
+        const uint scanline = v_scanline - (MODE_V_TOTAL_LINES - MODE_V_ACTIVE_LINES);
+        ch->read_addr = (uintptr_t)&linebuffer[(scanline % RGB_LINE_BUFFERS) * MODE_H_ACTIVE_PIXELS];
+        ch->transfer_count = MODE_H_ACTIVE_PIXELS;
         vactive_cmdlist_posted = false;
     }
 
@@ -189,17 +191,29 @@ void __scratch_x("") dma_irq_handler(void)
 
 void __not_in_flash_func(out_core1_main)(void)
 {
-    // Configure HSTX's TMDS encoder for RGB332
-    hstx_ctrl_hw->expand_tmds = 2 << HSTX_CTRL_EXPAND_TMDS_L2_NBITS_LSB | 0 << HSTX_CTRL_EXPAND_TMDS_L2_ROT_LSB | 2 << HSTX_CTRL_EXPAND_TMDS_L1_NBITS_LSB | 29 << HSTX_CTRL_EXPAND_TMDS_L1_ROT_LSB | 1 << HSTX_CTRL_EXPAND_TMDS_L0_NBITS_LSB | 26 << HSTX_CTRL_EXPAND_TMDS_L0_ROT_LSB;
+    // Configure HSTX's TMDS encoder for RGB888
+    hstx_ctrl_hw->expand_tmds = 7 << HSTX_CTRL_EXPAND_TMDS_L2_NBITS_LSB // R
+                                | 16 << HSTX_CTRL_EXPAND_TMDS_L2_ROT_LSB
+                                | 7 << HSTX_CTRL_EXPAND_TMDS_L1_NBITS_LSB // G
+                                | 8 << HSTX_CTRL_EXPAND_TMDS_L1_ROT_LSB
+                                | 7 << HSTX_CTRL_EXPAND_TMDS_L0_NBITS_LSB // B
+                                | 0 << HSTX_CTRL_EXPAND_TMDS_L0_ROT_LSB;
 
-    // Pixels (TMDS) come in 4 8-bit chunks. Control symbols (RAW) are an
-    // entire 32-bit word.
-    hstx_ctrl_hw->expand_shift = 4 << HSTX_CTRL_EXPAND_SHIFT_ENC_N_SHIFTS_LSB | 8 << HSTX_CTRL_EXPAND_SHIFT_ENC_SHIFT_LSB | 1 << HSTX_CTRL_EXPAND_SHIFT_RAW_N_SHIFTS_LSB | 0 << HSTX_CTRL_EXPAND_SHIFT_RAW_SHIFT_LSB;
+    // Pixels (TMDS) come in 3x 8-bit RGB + 1 byte padding.
+    // Control symbols (RAW) are an entire 32-bit word.
+    hstx_ctrl_hw->expand_shift = 1 << HSTX_CTRL_EXPAND_SHIFT_ENC_N_SHIFTS_LSB
+                                 | 0 << HSTX_CTRL_EXPAND_SHIFT_ENC_SHIFT_LSB
+                                 | 1 << HSTX_CTRL_EXPAND_SHIFT_RAW_N_SHIFTS_LSB
+                                 | 0 << HSTX_CTRL_EXPAND_SHIFT_RAW_SHIFT_LSB;
 
     // Serial output config: clock period of 5 cycles, pop from command
     // expander every 5 cycles, shift the output shiftreg by 2 every cycle.
     hstx_ctrl_hw->csr = 0;
-    hstx_ctrl_hw->csr = HSTX_CTRL_CSR_EXPAND_EN_BITS | 5u << HSTX_CTRL_CSR_CLKDIV_LSB | 5u << HSTX_CTRL_CSR_N_SHIFTS_LSB | 2u << HSTX_CTRL_CSR_SHIFT_LSB | HSTX_CTRL_CSR_EN_BITS;
+    hstx_ctrl_hw->csr = HSTX_CTRL_CSR_EXPAND_EN_BITS
+                        | 5u << HSTX_CTRL_CSR_CLKDIV_LSB
+                        | 5u << HSTX_CTRL_CSR_N_SHIFTS_LSB
+                        | 2u << HSTX_CTRL_CSR_SHIFT_LSB
+                        | HSTX_CTRL_CSR_EN_BITS;
 
     // Note we are leaving the HSTX clock at the SDK default of 125 MHz; since
     // we shift out two bits per HSTX clock cycle, this gives us an output of
@@ -225,7 +239,8 @@ void __not_in_flash_func(out_core1_main)(void)
         int bit = lane_to_output_bit[lane];
         // Output even bits during first half of each HSTX cycle, and odd bits
         // during second half. The shifter advances by two bits each cycle.
-        uint32_t lane_data_sel_bits = (lane * 10) << HSTX_CTRL_BIT0_SEL_P_LSB | (lane * 10 + 1) << HSTX_CTRL_BIT0_SEL_N_LSB;
+        uint32_t lane_data_sel_bits = (lane * 10) << HSTX_CTRL_BIT0_SEL_P_LSB
+                                      | (lane * 10 + 1) << HSTX_CTRL_BIT0_SEL_N_LSB;
         // The two halves of each pair get identical data, but one pin is inverted.
         hstx_ctrl_hw->bit[bit] = lane_data_sel_bits;
         hstx_ctrl_hw->bit[bit + 1] = lane_data_sel_bits | HSTX_CTRL_BIT0_INV_BITS;
@@ -292,6 +307,13 @@ void out_init(void)
     sleep_ms(10);
     set_sys_clock_khz(MAIN_SYS_CLOCK_KHZ, true);
     main_reclock();
+
+    // TODO: remove this initialization
+    for (unsigned i = 0; i < MODE_H_ACTIVE_PIXELS; ++i)
+    {
+        linebuffer[i] = i % 256 + ((i / 256) << 14);
+        linebuffer[i + MODE_H_ACTIVE_PIXELS] = (255 - (i % 256)) << 16;
+    }
 
     multicore_launch_core1(out_core1_main);
 }
