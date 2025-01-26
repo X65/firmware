cfb5e4f61c0fb454fe96e2a2f43571400bbcad80
Tomasz Sterna <tomasz@sterna.link>
2025-01-26 15:35:12 +0100

    Generate MODE2 demo data

--- src/ria/misc/bins.c
+++ src/ria/misc/bins.c
@@ -2,10 +2,22 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "../cgia/example_data.h"
 #include "../cgia/font_8.h"
+#include "./example_data.h"
 
 #define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
+
+void fputw(uint16_t word, FILE *file)
+{
+    fputc(word & 0xFF, file);
+    fputc(word >> 8, file);
+}
+
+void fput_header(uint16_t offset, uint16_t size, FILE *file)
+{
+    fputw(offset, file);            // from
+    fputw(offset + size - 1, file); // to
+}
 
 int main(void)
 {
@@ -19,38 +31,68 @@ int main(void)
 
     file = fopen("text_mode_dl.xex", "w");
     assert(file);
-    fputc(0xff, file);
-    fputc(0xff, file);
-    fputc(text_mode_dl_offset & 0xFF, file);
-    fputc(text_mode_dl_offset >> 8, file);
-    fputc((text_mode_dl_offset + NELEMS(text_mode_dl) - 1) & 0xFF, file);
-    fputc((text_mode_dl_offset + NELEMS(text_mode_dl) - 1) >> 8, file);
+    fputw(0xffff, file);                                          // file header
+    fput_header(text_mode_dl_offset, NELEMS(text_mode_dl), file); // block header
     for (size_t i = 0; i < NELEMS(text_mode_dl); ++i)
         fputc(text_mode_dl[i], file);
     fclose(file);
 
     file = fopen("affine_mode_dl.xex", "w");
     assert(file);
-    fputc(0xff, file);
-    fputc(0xff, file);
-    fputc(affine_mode_dl_offset & 0xFF, file);
-    fputc(affine_mode_dl_offset >> 8, file);
-    fputc((affine_mode_dl_offset + NELEMS(affine_mode_dl) - 1) & 0xFF, file);
-    fputc((affine_mode_dl_offset + NELEMS(affine_mode_dl) - 1) >> 8, file);
+    fputw(0xffff, file);
+    fput_header(affine_mode_dl_offset, NELEMS(affine_mode_dl), file);
     for (size_t i = 0; i < NELEMS(affine_mode_dl); ++i)
         fputc(affine_mode_dl[i], file);
     fclose(file);
 
     file = fopen("mixed_mode_dl.xex", "w");
     assert(file);
-    fputc(0xff, file);
-    fputc(0xff, file);
-    fputc(mixed_mode_dl_offset & 0xFF, file);
-    fputc(mixed_mode_dl_offset >> 8, file);
-    fputc((mixed_mode_dl_offset + NELEMS(mixed_mode_dl) - 1) & 0xFF, file);
-    fputc((mixed_mode_dl_offset + NELEMS(mixed_mode_dl) - 1) >> 8, file);
+    fputw(0xffff, file);
+    fput_header(mixed_mode_dl_offset, NELEMS(mixed_mode_dl), file);
     for (size_t i = 0; i < NELEMS(mixed_mode_dl); ++i)
         fputc(mixed_mode_dl[i], file);
+    fclose(file);
+
+    file = fopen("text_mode_cl.xex", "w");
+    assert(file);
+    fputw(0xffff, file);
+    fput_header(0x0000, 40 * 25, file); // MS
+    for (size_t i = 0; i < 40 * 25; ++i)
+        if (i >= 10 * 40 && i < 15 * 40)
+            switch (i / 40)
+            {
+            case 10:
+            case 14:
+                fputc(255, file);
+                break;
+            case 12:
+                fputc(text_mode_hello[i % 40], file);
+                break;
+            default:
+                fputc(0, file);
+            }
+        else
+            fputc(i & 0xFF, file);
+    fput_header(0x1000, 40 * 25, file); // CS
+    for (size_t i = 0; i < 40 * 25; ++i)
+        if (i >= 10 * 40 && i < 15 * 40)
+            switch (i / 40)
+            {
+            case 10:
+            case 14:
+                fputc(0x98 + (i % 40), file);
+                break;
+            default:
+                fputc(text_mode_fg_color, file);
+            }
+        else
+            fputc(i & 0xFF, file);
+    fput_header(0x2000, 40 * 25, file); // BS
+    for (size_t i = 0; i < 40 * 25; ++i)
+        if (i >= 10 * 40 && i < 15 * 40)
+            fputc(text_mode_bg_color, file);
+        else
+            fputc(0xFF - (i & 0xFF), file);
     fclose(file);
 
     return 0;
---
+++ src/ria/misc/example_data.h
@@ -0,0 +1,96 @@
+#include "sys/types.h"
+
+static const uint16_t text_mode_fg_color = 150; // 0x96
+static const uint16_t text_mode_bg_color = 145; // 0x91
+static const char *text_mode_hello = "              Hello World!              ";
+static const uint16_t text_mode_video_offset = 0x0000;
+static const uint16_t text_mode_color_offset = 0x1000;
+static const uint16_t text_mode_bkgnd_offset = 0x2000;
+static const uint16_t text_mode_chrgn_offset = 0x3000;
+static const uint16_t text_mode_dl_offset = 0x3800;
+static const uint8_t __attribute__((aligned(4))) text_mode_dl[] = {
+    0x70, 0x70, 0x30,                                     // 2x 8 + 1x 4 of empty background lines
+    0xF3, 0x00, 0x00, 0x00, 0x10, 0x00, 0x20, 0x00, 0x30, // LMS + LFS + LBS + LCG
+    0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,       // 8x MODE2
+    0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,       // 8x MODE2
+    0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,       // 8x MODE2
+    0x0A,                                                 // 1x MODE2
+    0x82, 0x00, 0x38                                      // JMP to begin of DL and wait for Vertical BLank
+};
+static const uint8_t __attribute__((aligned(4))) text80_mode_dl[] = {
+    0xF3,                                                                    // LMS + LFS + LBS + LCG
+    (text_mode_video_offset & 0xFF), ((text_mode_video_offset >> 8) & 0xFF), // LMS
+    (text_mode_color_offset & 0xFF), ((text_mode_color_offset >> 8) & 0xFF), // LFS
+    (text_mode_bkgnd_offset & 0xFF), ((text_mode_bkgnd_offset >> 8) & 0xFF), // LBS
+    (text_mode_chrgn_offset & 0xFF), ((text_mode_chrgn_offset >> 8) & 0xFF), // LCG
+    0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08,                          // 8x MODE0
+    0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08,                          // 8x MODE0
+    0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08,                          // 8x MODE0
+    0x08, 0x08, 0x08, 0x08, 0x08, 0x08,                                      // 6x MODE0
+    0x82, 0x00, 0x38                                                         // JMP to begin of DL and wait for Vertical BLank
+};
+
+#define EXAMPLE_SPRITE_WIDTH   4
+#define EXAMPLE_SPRITE_HEIGHT  26
+#define EXAMPLE_SPRITE_COLOR_1 0
+#define EXAMPLE_SPRITE_COLOR_2 23
+#define EXAMPLE_SPRITE_COLOR_3 10
+uint8_t __attribute__((aligned(4))) example_sprite_data[EXAMPLE_SPRITE_WIDTH * EXAMPLE_SPRITE_HEIGHT] = {
+    0b00000000, 0b00010101, 0b01000000, 0b00000000, //
+    0b00000000, 0b01011111, 0b11010100, 0b00000000, //
+    0b00000000, 0b01111111, 0b11111101, 0b00000000, //
+    0b00000001, 0b01010101, 0b01111111, 0b01000000, //
+    0b00000101, 0b01010101, 0b01011111, 0b11010000, //
+    0b00000101, 0b10101010, 0b10100101, 0b11010000, //
+    0b00000001, 0b10011001, 0b10101001, 0b01010100, //
+    0b00000001, 0b10011001, 0b10101001, 0b01100100, //
+    0b00000110, 0b10101010, 0b10100101, 0b01101001, //
+    0b00000110, 0b10101010, 0b01101001, 0b10101001, //
+    0b00010101, 0b10100101, 0b01011010, 0b10100100, //
+    0b00000001, 0b01010101, 0b10101010, 0b01010000, //
+    0b00000000, 0b01101010, 0b10100101, 0b01000000, //
+    0b00010100, 0b01010101, 0b01011111, 0b11010000, //
+    0b01101001, 0b01111101, 0b01111111, 0b11110100, //
+    0b01100101, 0b11111101, 0b11010101, 0b11111101, //
+    0b01100111, 0b11110101, 0b01101010, 0b01111101, //
+    0b00010111, 0b11010101, 0b10101010, 0b10011101, //
+    0b00011001, 0b01101001, 0b10101010, 0b10010100, //
+    0b01111101, 0b01101001, 0b01101010, 0b01010000, //
+    0b01111111, 0b01010101, 0b01010101, 0b11110100, //
+    0b01111111, 0b01010101, 0b01010101, 0b01110101, //
+    0b01111111, 0b01010101, 0b01010101, 0b01111101, //
+    0b01011111, 0b01000000, 0b01010101, 0b01111101, //
+    0b00010101, 0b00000000, 0b00000001, 0b01111101, //
+    0b00000000, 0b00000000, 0b00000000, 0b00010100, //
+};
+
+static const uint16_t affine_mode_video_offset = 0x0000;
+static const uint16_t affine_mode_dl_offset = 0xF800;
+static const uint8_t __attribute__((aligned(4))) affine_mode_dl[] = {
+    0x13, 0x00, 0x00, // LMS
+    0x0F, 0x0F,       // 2x MODE7
+    0x82, 0x00, 0xF8  // JMP to begin of DL and wait for Vertical BLank
+};
+static const uint16_t mixed_video_offset = 0xEA00;
+static const uint16_t mixed_color_offset = 0xEC00;
+static const uint16_t mixed_bkgnd_offset = 0xEE00;
+static const uint16_t mixed_chrgn_offset = 0xE200;
+static const uint16_t mixed_mode_dl_offset = 0xF800;
+static const uint8_t __attribute__((aligned(4))) mixed_mode_dl[] = {
+    0x13, 0x00, 0x00,                                                // LMS
+    0x14, 0x00,                                                      // border_columns = 0
+    0x24, 0x9f,                                                      // row_height = 159
+    0x34, (7 << 4) | 7,                                              // texture_bits = 7,7
+    0x0F,                                                            // MODE7
+    0x70,                                                            // 8x empty lines
+    0xF3,                                                            // LMS + LFS + LBS + LCG
+    (mixed_video_offset & 0xFF), ((mixed_video_offset >> 8) & 0xFF), // LMS
+    (mixed_color_offset & 0xFF), ((mixed_color_offset >> 8) & 0xFF), // LFS
+    (mixed_bkgnd_offset & 0xFF), ((mixed_bkgnd_offset >> 8) & 0xFF), // LBS
+    (mixed_chrgn_offset & 0xFF), ((mixed_chrgn_offset >> 8) & 0xFF), // LCG
+    0x14, 0x04,                                                      // border_columns = 4
+    0x24, 0x07,                                                      // row_height = 7
+    0x34, 0x00,                                                      // stride = 0
+    0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,                  // 8x MODE2
+    0x82, 0x00, 0xF8                                                 // JMP to begin of DL and wait for Vertical BLank
+};
