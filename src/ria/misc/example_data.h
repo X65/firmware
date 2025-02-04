0e4f8c63fc2bd26f252da53d0b9977180437ca01
Tomasz Sterna <tomasz@sterna.link>
2025-02-04 09:44:03 +0100

    Move affine and mixed DL to F000

--- src/ria/misc/example_data.h
+++ src/ria/misc/example_data.h
@@ -1,5 +1,7 @@
 #include "sys/types.h"
 
+#define split16(addr) (addr & 0xFF), ((addr >> 8) & 0xFF)
+
 static const uint16_t text_mode_fg_color = 150; // 0x96
 static const uint16_t text_mode_bg_color = 145; // 0x91
 static const char *text_mode_hello = "              Hello World!              ";
@@ -15,19 +17,19 @@ static const uint8_t __attribute__((aligned(4))) text_mode_dl[] = {
     0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,       // 8x MODE2
     0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,       // 8x MODE2
     0x0A,                                                 // 1x MODE2
-    0x82, 0x00, 0x38                                      // JMP to begin of DL and wait for Vertical BLank
+    0x82, split16(text_mode_dl_offset)                    // JMP to begin of DL and wait for Vertical BLank
 };
 static const uint8_t __attribute__((aligned(4))) text80_mode_dl[] = {
-    0xF3,                                                                    // LMS + LFS + LBS + LCG
-    (text_mode_video_offset & 0xFF), ((text_mode_video_offset >> 8) & 0xFF), // LMS
-    (text_mode_color_offset & 0xFF), ((text_mode_color_offset >> 8) & 0xFF), // LFS
-    (text_mode_bkgnd_offset & 0xFF), ((text_mode_bkgnd_offset >> 8) & 0xFF), // LBS
-    (text_mode_chrgn_offset & 0xFF), ((text_mode_chrgn_offset >> 8) & 0xFF), // LCG
-    0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08,                          // 8x MODE0
-    0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08,                          // 8x MODE0
-    0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08,                          // 8x MODE0
-    0x08, 0x08, 0x08, 0x08, 0x08, 0x08,                                      // 6x MODE0
-    0x82, 0x00, 0x38                                                         // JMP to begin of DL and wait for Vertical BLank
+    0xF3,                                           // LMS + LFS + LBS + LCG
+    split16(text_mode_video_offset),                // LMS
+    split16(text_mode_color_offset),                // LFS
+    split16(text_mode_bkgnd_offset),                // LBS
+    split16(text_mode_chrgn_offset),                // LCG
+    0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, // 8x MODE0
+    0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, // 8x MODE0
+    0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, // 8x MODE0
+    0x08, 0x08, 0x08, 0x08, 0x08, 0x08,             // 6x MODE0
+    0x82, split16(text_mode_dl_offset)              // JMP to begin of DL and wait for Vertical BLank
 };
 
 #define EXAMPLE_SPRITE_WIDTH   4
@@ -65,32 +67,32 @@ uint8_t __attribute__((aligned(4))) example_sprite_data[EXAMPLE_SPRITE_WIDTH * E
 };
 
 static const uint16_t affine_mode_video_offset = 0x0000;
-static const uint16_t affine_mode_dl_offset = 0xF800;
+static const uint16_t affine_mode_dl_offset = 0xF000;
 static const uint8_t __attribute__((aligned(4))) affine_mode_dl[] = {
-    0x13, 0x00, 0x00, // LMS
-    0x0F, 0x0F,       // 2x MODE7
-    0x82, 0x00, 0xF8  // JMP to begin of DL and wait for Vertical BLank
+    0x13, 0x00, 0x00,                    // LMS
+    0x0F, 0x0F,                          // 2x MODE7
+    0x82, split16(affine_mode_dl_offset) // JMP to begin of DL and wait for Vertical BLank
 };
 static const uint16_t mixed_video_offset = 0xEA00;
 static const uint16_t mixed_color_offset = 0xEC00;
 static const uint16_t mixed_bkgnd_offset = 0xEE00;
 static const uint16_t mixed_chrgn_offset = 0xE200;
-static const uint16_t mixed_mode_dl_offset = 0xF800;
+static const uint16_t mixed_mode_dl_offset = 0xF000;
 static const uint8_t __attribute__((aligned(4))) mixed_mode_dl[] = {
-    0x13, 0x00, 0x00,                                                // LMS
-    0x14, 0x00,                                                      // border_columns = 0
-    0x24, 0x9f,                                                      // row_height = 159
-    0x34, (7 << 4) | 7,                                              // texture_bits = 7,7
-    0x0F,                                                            // MODE7
-    0x70,                                                            // 8x empty lines
-    0xF3,                                                            // LMS + LFS + LBS + LCG
-    (mixed_video_offset & 0xFF), ((mixed_video_offset >> 8) & 0xFF), // LMS
-    (mixed_color_offset & 0xFF), ((mixed_color_offset >> 8) & 0xFF), // LFS
-    (mixed_bkgnd_offset & 0xFF), ((mixed_bkgnd_offset >> 8) & 0xFF), // LBS
-    (mixed_chrgn_offset & 0xFF), ((mixed_chrgn_offset >> 8) & 0xFF), // LCG
-    0x14, 0x04,                                                      // border_columns = 4
-    0x24, 0x07,                                                      // row_height = 7
-    0x34, 0x00,                                                      // stride = 0
-    0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A,                  // 8x MODE2
-    0x82, 0x00, 0xF8                                                 // JMP to begin of DL and wait for Vertical BLank
+    0x13, 0x00, 0x00,                               // LMS
+    0x14, 0x00,                                     // border_columns = 0
+    0x24, 0x9f,                                     // row_height = 159
+    0x34, (7 << 4) | 7,                             // texture_bits = 7,7
+    0x0F,                                           // MODE7
+    0x70,                                           // 8x empty lines
+    0xF3,                                           // LMS + LFS + LBS + LCG
+    split16(mixed_video_offset),                    // LMS
+    split16(mixed_color_offset),                    // LFS
+    split16(mixed_bkgnd_offset),                    // LBS
+    split16(mixed_chrgn_offset),                    // LCG
+    0x14, 0x04,                                     // border_columns = 4
+    0x24, 0x07,                                     // row_height = 7
+    0x34, 0x00,                                     // stride = 0
+    0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, // 8x MODE2
+    0x82, split16(mixed_mode_dl_offset)             // JMP to begin of DL and wait for Vertical BLank
 };
