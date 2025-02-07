fd6514f15de5315eb880bcd62c62ec2357468148
Tomasz Sterna <tomasz@sterna.link>
2025-02-07 17:54:21 +0100

    Move DVI timings to out.h

--- src/ria/sys/out.c
+++ src/ria/sys/out.c
@@ -22,34 +22,6 @@
 #include <limits.h>
 #include <stdio.h>
 
-/** https://retrocomputing.stackexchange.com/a/13872
-> In standard bitmap mode the C64 outputs 320 pixels in 40µs.
-> The visible portion of a line is ~52µs; in 60Hz regions ~240 lines
-> are considered 'visible', but in PAL regions it's ~288 lines.
-> So if there were no borders, there'd be around 52/40*320 = 416 pixels
-> across the visible portion of a line.
-*/
-
-// ANTIC generates 32/40/48 column text mode => max 256/320/384 px map mode
-// ANTIC supports up to 240 Display List instructions
-// With pixel-doubling this gives 768x480 mode, which has nice 16:10 aspect ratio
-// My TV recognizes this as 480p60 :-D
-// Timings computed using https://tomverbeure.github.io/video_timings_calculator
-#define MODE_H_SYNC_POLARITY 0
-#define MODE_H_FRONT_PORCH   24
-#define MODE_H_SYNC_WIDTH    72
-#define MODE_H_BACK_PORCH    96
-#define MODE_H_ACTIVE_PIXELS 768
-#define MODE_V_SYNC_POLARITY 1
-#define MODE_V_FRONT_PORCH   3
-#define MODE_V_SYNC_WIDTH    6
-#define MODE_V_BACK_PORCH    11
-#define MODE_V_ACTIVE_LINES  480
-#define MODE_BIT_CLK_KHZ     287500
-
-#define FB_H_REPEAT 1
-#define FB_V_REPEAT 2
-
 // ----------------------------------------------------------------------------
 #define LINE_BUFFER_PADDING (-SCHAR_MIN) // maximum scroll of signed 8 bit
 // RGB line buffers
@@ -373,7 +345,10 @@ void out_reclock(void)
                     0,
                     CLOCKS_CLK_HSTX_CTRL_AUXSRC_VALUE_CLK_SYS,
                     clock_get_hz(clk_sys),
-                    MODE_BIT_CLK_KHZ * KHZ / 2 /*DDR*/);
+                    MODE_BIT_CLK_KHZ * KHZ
+                        * 10 // TMDS symbol bits
+                        / 2  // DDR - two bits per clock on each edge
+    );
 }
 
 void out_init(void)
--- src/ria/sys/out.h
+++ src/ria/sys/out.h
@@ -7,8 +7,36 @@
 #ifndef _OUT_H_
 #define _OUT_H_
 
-#define FRAME_WIDTH  768
-#define FRAME_HEIGHT 240
+/** https://retrocomputing.stackexchange.com/a/13872
+> In standard bitmap mode the C64 outputs 320 pixels in 40µs.
+> The visible portion of a line is ~52µs; in 60Hz regions ~240 lines
+> are considered 'visible', but in PAL regions it's ~288 lines.
+> So if there were no borders, there'd be around 52/40*320 = 416 pixels
+> across the visible portion of a line.
+*/
+
+// ANTIC generates 32/40/48 column text mode => max 256/320/384 px map mode
+// ANTIC supports up to 240 Display List instructions
+// With pixel-doubling this gives 768x480 mode, which has nice 16:10 aspect ratio
+// My TV recognizes this as 480p60 :-D
+// Timings computed using https://tomverbeure.github.io/video_timings_calculator
+#define MODE_H_SYNC_POLARITY 0
+#define MODE_H_FRONT_PORCH   24
+#define MODE_H_SYNC_WIDTH    72
+#define MODE_H_BACK_PORCH    96
+#define MODE_H_ACTIVE_PIXELS 768
+#define MODE_V_SYNC_POLARITY 1
+#define MODE_V_FRONT_PORCH   3
+#define MODE_V_SYNC_WIDTH    6
+#define MODE_V_BACK_PORCH    11
+#define MODE_V_ACTIVE_LINES  480
+#define MODE_BIT_CLK_KHZ     28750
+
+#define FB_H_REPEAT 2
+#define FB_V_REPEAT 2
+
+#define FRAME_WIDTH  (MODE_H_ACTIVE_PIXELS / FB_H_REPEAT)
+#define FRAME_HEIGHT (MODE_V_ACTIVE_LINES / FB_V_REPEAT)
 
 void out_init(void);
 void out_reclock(void);
