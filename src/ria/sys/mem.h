picocomputer/rp6502

improve HID and MSC drivers, add hub support

src/ria/sys/mem.h
@@ -29,7 +29,7 @@ extern volatile size_t xstack_ptr;
 
 // RIA registers are located at the bottom of cpu1 stack.
 // cpu1 runs the action loop and uses very little stack.
-extern uint8_t regs[0x20];
+extern volatile uint8_t regs[0x20];
 #define REGS(addr) regs[(addr) & 0x1F]
 #define REGSW(addr) ((uint16_t *)&REGS(addr))[0]
 #if PICO_RP2040 == 1

X65/firmware

src/ria/sys/mem.h
@@ -27,7 +27,7 @@ extern volatile size_t xstack_ptr;
 
 // RIA registers are located at the bottom of cpu1 stack.
 // cpu1 runs the action loop and uses very little stack.
-extern uint8_t regs[0x40];
+extern volatile uint8_t regs[0x40];
 #define REGS(addr)  regs[(addr) & 0x3F]
 #define REGSW(addr) ((uint16_t *)&REGS(addr))[0]
 #if PICO_RP2040 == 1