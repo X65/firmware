678f832eae8aa3ad5286dd142079f66568608202
Tomasz Sterna <tomasz@sterna.link>
2025-12-29 13:28:25 +0100

    Skeleton for SGU-1

--- src/sound/hw.h
+++ src/sound/hw.h
@@ -10,29 +10,40 @@
 /* All pin assignments
  */
 
-#define SPU_LED_PIN 25
+#define SPU_RGB_LED_PIN 25
+
+#define COM_UART_TX_PIN 0
+#define COM_UART_RX_PIN (COM_UART_TX_PIN + 1)
+
+#define AUD_SPI_PIN_BASE 20
+#define AUD_SPI_RX_PIN   (AUD_SPI_PIN_BASE + 0)
+#define AUD_SPI_CS_PIN   (AUD_SPI_PIN_BASE + 1)
+#define AUD_SPI_SCK_PIN  (AUD_SPI_PIN_BASE + 2)
+#define AUD_SPI_TX_PIN   (AUD_SPI_PIN_BASE + 3)
 
 #define AUD_I2S_PIN_BASE  44
 #define AUD_I2S_DIN_PIN   (AUD_I2S_PIN_BASE + 0)
 #define AUD_I2S_SCLK_PIN  (AUD_I2S_PIN_BASE + 1)
 #define AUD_I2S_LRCLK_PIN (AUD_I2S_PIN_BASE + 2)
-#define AUD_I2S_DOUT_PIN  38 // WARNING: this PIN needs to be in the same GPIO bank as AUD_I2S_PIN_BASE
+#define AUD_I2S_DOUT_PIN  38 // WARNING: this PIN must be in the same GPIO bank as AUD_I2S_PIN_BASE
 
-#define EXT_I2C_SDA_PIN 36
-#define EXT_I2C_SCL_PIN 37
+#define EXT_I2C_SDA_PIN 16
+#define EXT_I2C_SCL_PIN 17
 
 /* All resource assignments
  */
+
+#define COM_UART_INTERFACE uart0
+#define COM_UART_BAUDRATE  115200
 
 // DAC chip I2S
 #define AUD_I2S_PIO pio2
 #define AUD_I2S_SM  0
 
-// Extension/External 3.3V I2C bus
+// CODEC I2C bus
 #define EXT_I2C          i2c0
 #define EXT_I2C_BAUDRATE (400 * 1000)
 
-#define IOE_I2C_ADDRESS 0x20 // Address of I/O Extender on I2C bus
 #define MIX_I2C_ADDRESS 0x40 // Address of Mixer on I2C bus
 #define I2S_I2C_ADDRESS 0x0A // Address of I2S DAC/ADC on I2C bus
 
